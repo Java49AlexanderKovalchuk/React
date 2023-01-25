07984305dc5976c151a528c9318d90f02038a3f0
AlexKovalchuk <kovalchuk111@gmail.com>
2023-01-25 15:46:01 +0200

    HW #35 dynamic form of timers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
+import React from 'react';
 import { Input } from './components/Input';
 import { Timer } from './components/Timer';
 
 
 function App() {
-  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column"}
-  const flexRow: React.CSSProperties = {
-    display: "flex", flexDirection: 'row', justifyContent: 'space-around',
-    width: "60vw", marginTop: "4vh"
+
+  const properties: React.CSSProperties = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
   }
-  
-  return <div style={flexColumn}>
-    <div style={flexRow}>
-      <Timer cityOrCountry={"Midway"}></Timer>
-      <Timer cityOrCountry={"Iceland"}></Timer>
-    </div>
-    <div style={flexRow}>
-      <Timer cityOrCountry={"Israel"}></Timer>
-      <Timer cityOrCountry={"Tallinn"}></Timer>
-    </div>
-  </div>
-  
-  // function processInput(value: string): string {
-  //   let res: string = '';
-  //   if(value.toLowerCase().includes('hello')) {
-  //   res = "get tired from HELLO. it's forbidden word";
-  //   }
-  //   return res;
-  // }
-  // return <Input inputId={'input-1'} inputProcess={processInput}
-  // />
-  // return <Rand someid={'id-1'} ></Rand>
+  const propertiesDivTimer: React.CSSProperties = {
+    border: "solid black 1px", marginRight: "2vw", marginTop: "5vh", width: "45vw"
+  }
+
+  //---getting timer engine--
+  const [time, setTime] = React.useState(new Date());
+  function tick() {
+    console.log("tick");
+    setTime(new Date());
+  }
+  React.useEffect(() => {
+    const interval = setInterval(tick, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  //-----------
+
+  const [placesTimes, setPlacesTimes] = React.useState<string[]>([]);
+
+  function creatingDivs(value: string): string {
+    const placesTimes: string[] = value.split("#");
+    if (placesTimes.length % 2 == 0) {
+      setPlacesTimes(placesTimes.slice());
+      return '';
+    }
+    else {
+      setPlacesTimes([]);
+      return `ERROR: entered number of names is ${placesTimes.length}. It must be even`
+    }
+
+  }
+  function getDivsTimers(placesTimes: string[]): JSX.Element[] {
+    return placesTimes.map(timer => <div style={propertiesDivTimer}>
+      <Timer time={time} cityOrCountry={timer} /></div>)
+
+  }
+  return <section style={{ display: 'flex', flexDirection: 'column' }}>
+    <Input placeHolder={'enter town or country names separated by #'} inputProcess={creatingDivs}></Input>
+    <section style={properties}>
+      {getDivsTimers(placesTimes)}
+    </section>
+  </section>
+
 }
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,40 +4,32 @@ import { Input } from "./Input";
 
 type TimerProps = {
     cityOrCountry: string;
+    time: Date;
 }
 
 export const Timer: React.FC<TimerProps> = (props) => {
-    const timeZoneIndex: number = timeZones.findIndex(tz => JSON.stringify(tz).   
-    includes(props.cityOrCountry));
-    const [timeZone, setTimeZone] = React.useState(timeZones[timeZoneIndex]?.name); 
-    const [timeZoneName, setTimeZoneName] = React.useState(timeZone ? props.cityOrCountry : "Israel");
-    const [time, setTime] = React.useState(new Date());
-    function tick() {
-        console.log("tick");
-        setTime(new Date());
-    }
-    React.useEffect(() => {
-        const interval = setInterval(tick, 1000);
-        return () => clearInterval(interval);
-    }, []);
+    const timeZoneIndex: number = timeZones.findIndex(tz => JSON.stringify(tz).
+        includes(props.cityOrCountry));
+    const [timeZone, setTimeZone] = React.useState(timeZones[timeZoneIndex]?.name);
+    const timeZoneName = React.useRef(timeZone ? props.cityOrCountry : "Israel");
 
     function processSityCountry(value: string): string {
         const index = timeZones.findIndex(tz => JSON.stringify(tz).includes(value));
         let res = '';
-        if(index < 0) {
+        if (index < 0) {
             res = `${value} is wrong city / country, please type again`;
         } else {
-            setTimeZoneName(value);
+            timeZoneName.current = value;
             setTimeZone(timeZones[index].name);
         }
-         return res;
+        return res;
     }
 
     return <div>
         <Input placeHolder={"Enter city or country"} inputProcess={processSityCountry} />
-        <h3 className="logo">Time in {timeZoneName}</h3>
+        <h3 className="logo" style={{ textAlign: "center" }}>Time in {timeZoneName.current}</h3>
         <label style={{ display: "block", textAlign: "center", fontSize: "2em" }}>
-            Time {time.toLocaleTimeString(undefined, { timeZone })}</label>
+            {props.time.toLocaleTimeString(undefined, { timeZone })}</label>
 
     </div>
 }
