e6e7b2d24631b9e2f628432db9a721cdfd13be6b
AlexKovalchuk <kovalchuk111@gmail.com>
2023-02-06 18:54:58 +0200

    cw39 init with authSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Life } from './components/Life';
-import lifeConfig from './config/life-game.json'
+import { CounterMultiply } from './components/CounterMultiply';
+import { CounterSquare } from './components/CounterSquare';
+import { CounterUpdater } from './components/CounterUpdater';
+import { Login } from './components/Login';
+import { Logout } from './components/Logout';
+import { useSelector } from 'react-redux';
+
 function App() {
+  const auth: boolean = useSelector<any, boolean>(state => state.auth.authenticated); 
 
-
-  return <div style={{display: 'flex', justifyContent: 'center',
-   alignItems: 'center'}}>
-    <Life dimensions={lifeConfig.dimensions} ticInterval={lifeConfig.ticInterval}></Life>
+  return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    {auth && <div>
+      <CounterUpdater></CounterUpdater>
+      <CounterSquare></CounterSquare>
+      <CounterMultiply></CounterMultiply>
+    </div>}
+    {auth && <Logout></Logout>}
+    {!auth && <Login></Login>}
   </div>
 
 }
---
+++ src/redux/counterSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    count: 10
+}
+const counterSlice = createSlice({
+    initialState: initialState,
+    name: 'counter',
+    reducers: {
+        increment: (state) => {
+            state.count++;
+        }, 
+        decrement: (state) => {
+            state.count--;
+        },
+        reset: state => {
+            state.count = initialState.count;
+        }
+    }
+})
+
+export const counterActions = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
