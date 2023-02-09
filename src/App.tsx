0d458a21508c1c1138cc7587aa27b317aed5b8ed
AlexKovalchuk <kovalchuk111@gmail.com>
2023-02-10 00:11:08 +0200

    CW - 40 Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
+
 import React from 'react';
-import { CounterMultiply } from './components/CounterMultiply';
-import { CounterSquare } from './components/CounterSquare';
-import { CounterUpdater } from './components/CounterUpdater';
-import { Login } from './components/Login';
-import { Logout } from './components/Logout';
-import { useSelector } from 'react-redux';
-import { Input } from './components/Input';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Layout } from './components/navigators/Layout';
+import { Products } from './components/navigators/Products';
+import { BreadProducts } from './components/pages/BreadProducts';
+import { Customers } from './components/pages/Customers';
+import { DairyProducts } from './components/pages/DairyProducts';
+import { Home } from './components/pages/Home';
+import { Orders } from './components/pages/Orders';
+import './App.css'
+
 
 function App() {
-  const userName: string = useSelector<any, string>(state => state.auth.authenticated);
-  const [operand, setOperand] = React.useState(1);
-  const [factor, setFactor] = React.useState(10);
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Home/>}></Route>
+        <Route path='customers' element={<Customers/>}/>
+        <Route path='orders' element={<Orders/>}></Route>
+        <Route path='products' element={<Products/>}>
+          <Route path='dairy' element={<DairyProducts/>}/>
+          <Route path='bread' element={<BreadProducts/>}/>
+        </Route>
 
-  function includeSubstring(str: string): boolean {
-    return str.includes("admin");
-  }
-  return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    {(userName) && <div>
-      <p>username: {userName}</p>
-      <Input placeHolder={'Enter operand'} inputProcess={function (value: string): string {
-        setOperand(+value);
-        return '';
-      }} ></Input>
-      <Input placeHolder={'Enter factor'} inputProcess={function (value: string): string {
-        setFactor(+value);
-        return '';
-      }}></Input>
-    </div>}
-    {(userName) && <div>
-      <CounterUpdater operand={operand}></CounterUpdater>
-      <CounterSquare></CounterSquare>
-      <CounterMultiply factor={factor}></CounterMultiply>
-    </div>}
-    {(userName) && <Logout></Logout>}
-    {(!userName) && <Login valid={includeSubstring}></Login>}
-  </div>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+ 
 
 }
 export default App;
