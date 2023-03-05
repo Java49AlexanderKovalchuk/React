272093d8df5b39e78f596907938505c755b9f207
AlexKovalchuk <kovalchuk111@gmail.com>
2023-03-05 17:29:00 +0200

    hw-46  my fin vers

--- src/components/navigators/NavigatorMobile.tsx
+++ src/components/navigators/NavigatorMobile.tsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigatorProps } from '../../model/NavigatorProps';
-import {
-    Box, Button, AppBar, Toolbar, Typography, Divider,
-    Tab,
-} from '@mui/material';
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Box, AppBar, Toolbar, Typography, Tab } from '@mui/material';
+import { Link, Outlet } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close'
 import { Drawer } from '@mui/material';
 import { useState } from "react";
-import { useNavigate } from 'react-router';
-
 
 export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
-    
+
     function getNavItems(routes: { path: string; label: string }[]): React.ReactNode {
         return routes.map((r, index) => <Tab component={Link} to={r.path}
             label={r.label} key={index} onClick={() => {
@@ -25,11 +19,6 @@ export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
     const [title, setTitle] = useState<string>("Employees' Company");
     const [openMenu, setOpenMenu] = useState<boolean>(false);
 
-
-    const handleCloseClick = () => {
-        setOpenMenu(false);
-    };
-
     const handleMenuClick = () => {
         setOpenMenu(true);
     };
@@ -38,24 +27,19 @@ export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
         <Box>
             <AppBar sx={{}}>
                 <Toolbar>
-                    <Button onClick={handleMenuClick}>
-                        <MenuIcon sx={{ color: 'white' }} />
-                    </Button>
+                    <MenuIcon onClick={handleMenuClick} sx={{ color: 'white' }} />
                     <Typography sx={{ width: '100%', textAlign: 'center' }}>
                         {title}
                     </Typography>
                 </Toolbar>
             </AppBar>
-            
             <Drawer open={openMenu} variant="temporary">
                 {getNavItems(routes)}
             </Drawer>
-
-            <Box sx={{mt:'20vh'}}>
+            <Box sx={{ mt: '20vh' }}>
                 <Outlet></Outlet>
             </Box>
         </Box>
     );
-
 }
 
